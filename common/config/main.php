f8d1f13bcf2fb867b23280d01742f8930f032960
SaintFurion <saintfurion56@gmail.com>
2023-08-31 21:55:36 +0500

    Развертывание проекта

---
+++ common/config/main.php
@@ -0,0 +1,24 @@
+<?php
+return [
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => \yii\caching\FileCache::class,
+        ],
+        'urlManager' => [
+            'class' => 'yii\web\UrlManager',
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                'about' => 'site/about',
+                'contact' => 'site/contact',
+                'login' => 'site/login',
+                '' => 'site/index'
+            ],
+        ],
+    ],
+];
